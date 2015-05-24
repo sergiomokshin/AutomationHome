ed357f7bf1ebbe6734b9d7427eb2317ce984eca8
Sergio Mokshin <sergio.mokshin@gmail.com>
2015-05-24 20:56:34 -0300

    DES

--- Testes/esp/esp.ino
+++ Testes/esp/esp.ino
@@ -1,47 +1,218 @@
-// Programa : Teste modulo wireless ESP8266
-// Autor : Arduino e Cia
+#include <SoftwareSerial.h>
+ 
+#define DEBUG true
+ 
+SoftwareSerial esp8266(12,11); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
+                             // This means that you need to connect the TX line from the esp to the Arduino's pin 2
+ 
+ 
+ /*
+192.168.0.12
 
-// Carrega as bibliotecas ESP8266 e SoftwareSerial
-#include "ESP8266.h"
-#include "SoftwareSerial.h"
-    
-// Cria uma serial nas portas 2 (RX) e 3 (TX)
-SoftwareSerial minhaSerial(2 , 3); 
-
-// Define que o modulo ira utilizar a serial minhaSerial
-ESP8266 wifi(minhaSerial);
-
-// Configuracao ID e senha da rede Wireless
-#define SSID        "NOME_DA_REDE"
-#define PASSWORD    "SENHA_DA_REDE"
-
-
+ 
+ 
+ 
+ */
+                             // and the RX line from the esp to the Arduino's pin 3
 void setup()
 {
-    Serial.begin(9600);
-}
+  Serial.begin(9600);
+  esp8266.begin(9600); // your esp's baud rate might be different
+  
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(9, OUTPUT);
 
-void loop(void)
+  digitalWrite(A0, LOW);    
+  digitalWrite(A1, LOW);    
+  digitalWrite(A2, LOW);    
+  digitalWrite(A3, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(9, LOW);
+       
+ 
+  analogWrite(6, 0);  
+  analogWrite(5, 0);     
+  analogWrite(3, 0);  
+   
+  sendCommand("AT+RST\r\n",2000,DEBUG); // reset module
+  sendCommand("AT+CWMODE=1\r\n",1000,DEBUG); // configure as access point
+  sendCommand("AT+CWJAP=\"SERGIO\",\"992581315\"\r\n",3000,DEBUG);
+  delay(10000);
+  sendCommand("AT+CIFSR\r\n",1000,DEBUG); // get ip address
+  sendCommand("AT+CIPMUX=1\r\n",1000,DEBUG); // configure for multiple connections
+  sendCommand("AT+CIPSERVER=1,80\r\n",1000,DEBUG); // turn on server on port 80
+  
+  Serial.println("Server Ready");
+}
+ 
+void loop()
 {
-    Serial.print("Inicializando modulo\r\n");
-    Serial.print("Versao do firmware: ");
-    Serial.println(wifi.getVersion().c_str());
-    // Define modo de operacao como STA (station)
-    if (wifi.setOprToStation()) {
-        Serial.print("Modo STA ok\r\n");
-    } else {
-        Serial.print("Erro ao definir modo STA !r\n");
+  if(esp8266.available()) // check if the esp is sending a message 
+  {
+ 
+    
+    if(esp8266.find("+IPD,"))
+    {
+     delay(1000); // wait for the serial buffer to fill up (read all the serial data)
+     // get the connection id so that we can then disconnect
+     int connectionId = esp8266.read()-48; // subtract 48 because the read() function returns 
+                                           // the ASCII decimal value and 0 (the first decimal number) starts at 48
+          
+     esp8266.find("pin="); // advance cursor to "pin="
+          
+     int pinNumber = (esp8266.read()-48); // get first number i.e. if the pin 13 then the 1st number is 1
+     int secondNumber = (esp8266.read()-48);
+     if(secondNumber>=0 && secondNumber<=9)
+     {
+      pinNumber*=10;
+      pinNumber +=secondNumber; // get second number, i.e. if the pin number is 13 then the 2nd number is 3, then add to the first number
+     }
+     
+     digitalWrite(pinNumber, !digitalRead(pinNumber)); // toggle pin    
+     
+     // build string that is send back to device that is requesting pin toggle
+     String content;
+     content = "Pin ";
+     content += pinNumber;
+     content += " is ";
+     
+     if(digitalRead(pinNumber))
+     {
+       content += "ON";
+     }
+     else
+     {
+       content += "OFF";
+     }
+     
+     sendHTTPResponse(connectionId,content);
+     
+     // make close command
+     String closeCommand = "AT+CIPCLOSE="; 
+     closeCommand+=connectionId; // append connection id
+     closeCommand+="\r\n";
+     
+     sendCommand(closeCommand,1000,DEBUG); // close connection
+    }
+  }
+}
+ 
+/*
+* Name: sendData
+* Description: Function used to send data to ESP8266.
+* Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
+* Returns: The response from the esp8266 (if there is a reponse)
+*/
+String sendData(String command, const int timeout, boolean debug)
+{
+    String response = "";
+    
+    int dataSize = command.length();
+    char data[dataSize];
+    command.toCharArray(data,dataSize);
+           
+    esp8266.write(data,dataSize); // send the read character to the esp8266
+    if(debug)
+    {
+      Serial.println("\r\n====== HTTP Response From Arduino ======");
+      Serial.write(data,dataSize);
+      Serial.println("\r\n========================================");
     }
     
-    // Conexao a rede especificada em SSID
-    if (wifi.joinAP(SSID, PASSWORD)) {
-        Serial.print("Conectado com sucesso a rede wireless\r\n");
-        Serial.print("IP: ");       
-        Serial.println(wifi.getLocalIP().c_str());
-    } else {
-        Serial.print("Erro ao conectar rede wireless !!!\r\n");
+    long int time = millis();
+    
+    while( (time+timeout) > millis())
+    {
+      while(esp8266.available())
+      {
+        
+        // The esp has data so display its output to the serial window 
+        char c = esp8266.read(); // read the next character.
+        response+=c;
+      }  
     }
     
-    Serial.print("*** Fim ***\r\n");
-    while(1){}
+    if(debug)
+    {
+      Serial.print(response);
+    }
+    
+    return response;
+}
+ 
+/*
+* Name: sendHTTPResponse
+* Description: Function that sends HTTP 200, HTML UTF-8 response
+*/
+void sendHTTPResponse(int connectionId, String content)
+{
+     
+     // build HTTP response
+     String httpResponse;
+     String httpHeader;
+     // HTTP Header
+     httpHeader = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n"; 
+     httpHeader += "Content-Length: ";
+     httpHeader += content.length();
+     httpHeader += "\r\n";
+     httpHeader +="Connection: close\r\n\r\n";
+     httpResponse = httpHeader + content + " "; // There is a bug in this code: the last character of "content" is not sent, I cheated by adding this extra space
+     sendCIPData(connectionId,httpResponse);
+}
+ 
+/*
+* Name: sendCIPDATA
+* Description: sends a CIPSEND=<connectionId>,<data> command
+*
+*/
+void sendCIPData(int connectionId, String data)
+{
+   String cipSend = "AT+CIPSEND=";
+   cipSend += connectionId;
+   cipSend += ",";
+   cipSend +=data.length();
+   cipSend +="\r\n";
+   sendCommand(cipSend,1000,DEBUG);
+   sendData(data,1000,DEBUG);
+}
+ 
+/*
+* Name: sendCommand
+* Description: Function used to send data to ESP8266.
+* Params: command - the data/command to send; timeout - the time to wait for a response; debug - print to Serial window?(true = yes, false = no)
+* Returns: The response from the esp8266 (if there is a reponse)
+*/
+String sendCommand(String command, const int timeout, boolean debug)
+{
+    String response = "";
+           
+    esp8266.print(command); // send the read character to the esp8266
+    
+    long int time = millis();
+    
+    while( (time+timeout) > millis())
+    {
+      while(esp8266.available())
+      {
+        
+        // The esp has data so display its output to the serial window 
+        char c = esp8266.read(); // read the next character.
+        response+=c;
+      }  
+    }
+    
+    if(debug)
+    {
+      Serial.print(response);
+    }
+    
+    return response;
 }
--- Testes/esp2/esp2.ino
+++ Testes/esp2/esp2.ino
@@ -1,70 +1,233 @@
-#include <SoftwareSerial.h>
-SoftwareSerial m (12, 11); // RX, TX
+/* ====== ESP8266 + Arduino Webserver ======
+ * 24.12.2014
+ * Simple arduino + esp8266 webserver 
+ * using AT commands and GET forms
+ * AT from http://www.ai-thinker.com/
+ * V0.9.2.2
+ * Chirila Cezar @epilepsynerd.wordpress.com
+ * Thanks to :
+ * Ray Wang @ Rayshobby LLC
+ * http://rayshobby.net/?p=9734
+ */
 
-const byte CH_PD=5;
-const byte RST=6;
-#define rede "SERGIO"
-#define senha "992581315"
+#define BUFFER_SIZE 512
+#define GET_SIZE 64
+//#define SSID  "Abraham Linksys"      
+//#define PASS  "mobydick"  
+#define PORT  "80"           
+#define dbg Serial  //For debugging purposes
+
+char buffer[BUFFER_SIZE];
+char get_s[GET_SIZE];
+char OKrn[] = "OK\r\n"; 
+
+
+#include <SoftwareSerial.h>
+
+//Software serial to communicate with the esp
+SoftwareSerial esp(11, 12); //RX, TX
+
+byte wait_for_esp_response(int timeout, char* term=OKrn) {
+  unsigned long t=millis();
+  bool found=false;
+  int i=0;
+  int len=strlen(term);
+  // wait for at most timeout milliseconds
+  // or if OK\r\n is found
+  while(millis()<t+timeout) {
+    if(esp.available()) {
+      buffer[i++]=esp.read();
+      if(i>=len) {
+        if(strncmp(buffer+i-len, term, len)==0) {
+          found=true;
+          break;
+        }
+      }
+    }
+  }
+  buffer[i]=0;
+  dbg.print(buffer);
+  return found;
+}
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("--------------");
-  pinMode(CH_PD,OUTPUT);
-  digitalWrite(CH_PD,HIGH);
-  pinMode(RST,OUTPUT);
-  digitalWrite(RST,LOW);
-  delay(100);
-  digitalWrite(RST,HIGH);
-  delay(1000);
-  m.begin(115200);
-  m.setTimeout(5000);
 
-  Serial.println("ESP8266 Demo - aguardando 1 segundo");
-  delay(1000);
-  Serial.println("Enviando RST");
-  m.println("AT+RST");  
-  recebeResposta();
-  recebeResposta(); // o reset tem 2 respostas - OK (antes) e ready (depois) 
-  Serial.println("Listando redes visiveis");
-  m.println("AT+CWLAP");  
-  recebeResposta();
-  Serial.println("Conectando-se a uma rede");
-  String comando="AT+CWJAP=\"";
-  comando+=rede;
-  comando+="\",\"";
-  comando+=senha;  
-  comando+="\"";
-  m.println(comando);  
-  recebeResposta();
-  Serial.println("Listando endereco IP (pode dar ping no segundo deles)");
-  m.println("AT+CIFSR");  
-  recebeResposta();
+  esp.begin(9600); //esp baud rate
+  dbg.begin(9600); //debugger baud rate
+
+  dbg.println("begin.");
+
+  setupWiFi(2,"","");
+
+  // print device IP address
+  dbg.print("device ip addr:");
+  esp.println("AT+CIFSR");
+  wait_for_esp_response(1000);
 }
 
 void loop() {
+  int ch_id, packet_len, lssid = 0, lpass = 0;
+  char *pb;  
+  if(read_till_eol()) {
+    if(strncmp(buffer, "+IPD,", 5)==0) {
+      // request: +IPD,ch,len:data
+      sscanf(buffer+5, "%d,%d", &ch_id, &packet_len);
+      if (packet_len > 0) {
+        // read serial until packet_len character received
+        // start from :
+        pb = buffer+5;
+        while(*pb!=':') pb++;
+        pb++;
+        get_data(pb, ch_id);
+      }
+    }
+  }
 }
 
-void recebeResposta() {
-  int limite=7000;
-  unsigned long chegada=millis();
-  boolean continuar=true; 
-  String S="";
-  unsigned long ultimochar=0;
-  while (continuar) { 
-    if (m.available()) {
-      char c = m.read();
-      ultimochar=millis();
-      S=S+c;
-      Serial.print(c);
-      if (c==10) {  // LF, fim da linha recebida
-        byte p=S.indexOf(13);
-        String S1=S.substring(0,p);
-        if (S1=="OK") continuar=false;
-        if (S1=="ready") continuar=false;
-        if (S1=="ERROR") continuar=false;
-        S="";
-      }  
-    }  
-    if (millis()-chegada > limite) continuar=false;
+void get_data(char *pb, int ch_id){
+  char *pget;
+  if (strncmp(pb, "GET /favicon.ico", 16) == 0) {
+    pb = pb+6;
+    wait_for_esp_response(1000);
   }
-} 
+
+  if (strncmp(pb, "GET /?", 6) == 0) {
+    pb = pb+6;
+    pget = get_s;
+    while(*pb!=' '){
+      *pget = *pb;
+      pb++; 
+      pget++;
+    }
+    sort_data();
+    wait_for_esp_response(1000);
+    dbg.println("-> serve homepage\n");
+    serve_homepage(ch_id);
+
+  }
+
+  else if (strncmp(pb, "GET /", 5) == 0) {
+    wait_for_esp_response(1000);
+    dbg.println("-> serve homepage\n");
+    serve_homepage(ch_id);
+  }
+}
+
+void sort_data(){
+  String _ssid ="";
+  String _pass ="";
+
+  char *pget;
+  pget = get_s;
+
+  pget = pget+5;
+
+  while(*pget!='&'){
+    _ssid += *pget;
+    pget++; 
+  }
+
+  pget = pget+10;
+  while(*pget!=' '){
+    _pass += *pget;
+    pget++; 
+  }
+  wait_for_esp_response(2000);
+  setupWiFi(1, _ssid, _pass);
+}
+
+void serve_homepage(int ch_id) {
+  String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n";
+
+  String content="";
+  // output the value of each analog input pin
+  content += "<form method=get>";
+  content += "<label>SSID</label><br>";
+  content += "<input  type='text' name='ssid' maxlength='30' size='15'><br>";
+  content += "<label>Password</label><br>";
+  content += "<input  type='password' name='password' maxlength='30' size='15'><br><br>";
+  content += "<input  type='submit' value='connect' >";
+  content += "</form>";
+
+  header += "Content-Length:";
+  header += (int)(content.length());
+  header += "\r\n\r\n";
+  esp.print("AT+CIPSEND=");
+  esp.print(ch_id);
+  esp.print(",");
+  esp.println(header.length()+content.length());
+  if(wait_for_esp_response(2000, "> ")) {
+    esp.print(header);
+    esp.print(content);
+  } 
+  else {
+    esp.print("AT+CIPCLOSE=");
+    esp.println(ch_id);
+  }
+}
+
+
+void setupWiFi(int mode, String SSID, String PASS) {
+  // try empty AT command
+  esp.println("AT");
+  wait_for_esp_response(1000);
+
+  // set mode 1 (client) or mode 2 (AP)
+  if(mode==1){
+    esp.println("AT+CWMODE=1");
+    wait_for_esp_response(1000);  
+  }
+  else if(mode==2){
+    esp.println("AT+CWMODE=2");
+    wait_for_esp_response(1000);  
+  }
+
+  // reset WiFi module
+  esp.print("AT+RST\r\n");
+  wait_for_esp_response(1500);
+  wait_for_esp_response(3000);
+
+  if(mode==1){
+    // join AP
+    esp.print("AT+CWJAP=\"");
+    esp.print(SSID);
+    esp.print("\",\"");
+    esp.print(PASS);
+    esp.println("\"");
+    // this may take a while, so wait for 5 seconds
+    wait_for_esp_response(5000);
+  }
+  // start server
+  esp.println("AT+CIPMUX=1");
+  wait_for_esp_response(1000);
+
+  esp.print("AT+CIPSERVER=1,"); // turn on TCP service
+  esp.println(PORT);
+  wait_for_esp_response(1000);
+
+}
+
+
+
+bool read_till_eol() {
+  static int i=0;
+  if(esp.available()) {
+    buffer[i++]=esp.read();
+    if(i==BUFFER_SIZE)  i=0;
+    if(i>1 && buffer[i-2]==13 && buffer[i-1]==10) {
+      buffer[i]=0;
+      i=0;
+      dbg.print(buffer);
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
