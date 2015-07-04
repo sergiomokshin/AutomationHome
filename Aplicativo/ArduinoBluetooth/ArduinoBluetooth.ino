fe70a9c26642fd5af916aef9aca55dd93f7aff0f
Sergio Mokshin <sergio.mokshin@gmail.com>
2015-07-04 15:50:55 -0300

    DES

--- Aplicativo/ArduinoBluetooth/ArduinoBluetooth.ino
+++ Aplicativo/ArduinoBluetooth/ArduinoBluetooth.ino
@@ -34,8 +34,19 @@ Automação Live - Abril/2015
 #include "Wire.h"
 #include <LiquidCrystal_I2C.h>	// For the LCD
 
-LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
-#define DS1307_I2C_ADDRESS 0x68
+
+
+//Endereços de LCDs 1602
+LiquidCrystal_I2C lcd(0x3F, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(0x27, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(32,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+
+//Endereços de LCDs 2004
+//LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 20 chars and 4 line display
+
+//Se necessário utilizar um I2C Scanner http://playground.arduino.cc/Main/I2cScanner
+
+#define DS1307_I2C_ADDRESS 0x68  
 
 #define PIN_S1 2
 #define PIN_S2 4
--- EthernetW5100/Arduino_EthernetW5100/Arduino_EthernetW5100.ino
+++ EthernetW5100/Arduino_EthernetW5100/Arduino_EthernetW5100.ino
@@ -14,7 +14,17 @@ Automação Live - Abril/2015
 #define DHTPIN 8
 #define DHTTYPE DHT11
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+//Endereços de LCDs 1602
+LiquidCrystal_I2C lcd(0x3F, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(0x27, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(32,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+
+//Endereços de LCDs 2004
+//LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 20 chars and 4 line display
+
+//Se necessário utilizar um I2C Scanner http://playground.arduino.cc/Main/I2cScanner
+
+
 #define DS1307_I2C_ADDRESS 0x68
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0xA6, 0x09 };
