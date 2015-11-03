42c548f0f06f51a57edb6de26a0d09db543728d7
Sergio Mokshin <sergio.mokshin@gmail.com>
2015-11-03 17:37:01 -0200

    DES

--- Aplicativo/ArduinoBluetooth/ArduinoBluetooth.ino
+++ Aplicativo/ArduinoBluetooth/ArduinoBluetooth.ino
@@ -36,7 +36,7 @@ Automação Live - Abril/2015
 
 
 //Endereços de LCDs 1602
-LiquidCrystal_I2C lcd(0x3F, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x3F, 16,2);// set the LCD address to 0x3F for a 16 chars and 2 line display
 //LiquidCrystal_I2C lcd(0x27, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
 //LiquidCrystal_I2C lcd(0x20,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
 
